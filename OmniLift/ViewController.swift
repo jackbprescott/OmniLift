f9c686e6dcde299a39704746ad9a3660cc984af3
Jack Prescott <jack@Jacks-MacBook-Pro.local>
2018-01-11 12:43:08 -0800

    Cleaning it up commit

--- OmniLift/ViewController.swift
+++ OmniLift/ViewController.swift
@@ -13,12 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //This comment is a test
-        let x = 43
-        print(x)
-        //Another comment
-        //Changing it more
-        var y = 10
     }
 
     override func didReceiveMemoryWarning() {
