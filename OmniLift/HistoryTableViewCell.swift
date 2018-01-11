5b32b98e2f4a11a697a33248d53d17b5833a71c9
Jack Prescott <jack@Jacks-MacBook-Pro.local>
2018-01-11 14:56:30 -0800

    Added files and table views to be worked on

---
+++ OmniLift/HistoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryTableViewCell.swift
+//  OmniLift
+//
+//  Created by Jack Prescott on 1/11/18.
+//  Copyright © 2018 Something Clever. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
